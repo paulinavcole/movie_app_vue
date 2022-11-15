674dcf5d2207e749fa812773a54d3b5278f7ef7b
Paulina Cole <lina.cole@gmail.com>
2022-11-14 22:07:37 -0500

    breaking bad api working

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+ import BreakingBadCardsVue from './components/BreakingBadCards.vue';
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <Suspense>
+      <template #default>
+        <BreakingBadCardsVue />
+      </template>
+      <template #fallback>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const { image, name, occupation } = defineProps([
+  "image",
+  "name",
+  "occupation",
+]);
+
+
+</script>
+<template>
+    <n-card>
+    <template #cover>
+      <img :src="image">
+    </template>
+    <h3>{{ name }}</h3>
+    <div class="jobs">
+        <p 
+            v-for="(job, index) in occupation"
+            :key="job"
+            
+        >
+            {{ job }}<span v-if="index < occupation.length-1">,&nbsp</span></p>
+    </div>
+  </n-card>
+</template>
+
+
+<style scoped>
+.n-card {
+    width: 200px;
+    margin:10px 20px;
+
+}
+.n-card img {
+    height: 250px
+}
+
+p {
+    font-size: 10px;
+}
+
+.jobs {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
